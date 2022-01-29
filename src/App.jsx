84f3c00db26e46f7adfb166f6089db750df8c67d
AL3M4N <HermesXVIII@gmail.com>
2022-01-29 15:02:51 -0500

    Archivo app eliminado y archivo index editado

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { TodoList } from "./components/TodoList";
-
-export function App() {
-    return <TodoList listas={[{ id:1, task:"Tarea 1",completed: false }]}/>;
-}
